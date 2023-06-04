3ac12317db5c2c0ab514d67bc43ee14bd5512537
Aaron <C-Shart@users.noreply.github.com>
2023-06-03 22:54:02 -0700

    New logic, refactor, random words Simplified and streamlined, and ready for more

--- words_typed_on_alternating_hands.py
+++ words_typed_on_alternating_hands.py
@@ -1,58 +1,42 @@
 # Find all words in a list that are typed using alternating hands on a QWERTY keyboard
+from english_words import get_english_words_set
+web2lowerset = get_english_words_set(['web2'], alpha=True, lower=True)
+wordlist = list(web2lowerset)
 
-import requests
+# TODO: lefthand, righthand values to sets or dicts
+# TODO: Selectable/configurable keyboard layouts
+lefthand = set("q", "a", "z", "w", "s", "x", "e", "d", "c", "r", "f", "v", "t", "g", "b")
+righthand = set("y", "h", "n", "u", "j", "m", "i", "k", "o", "l", "p")
 
-words = ["aaron", "Adam", "haughty", "Jeezy", "ambient", "skeptic"]
+final_length = 5                    # TODO: configurable
+sentence = []                       # final string of words
+cur_sentence_length = 0
+i = 0
 
-lefthand = ["q", "a", "z", "w", "s", "x", "e", "d", "c", "r", "f", "v", "t", "g", "b"]
-righthand = ["t", "g", "b", "y", "h", "n", "u", "j", "m", "i", "k", "o", "l", "p"]
+while cur_sentence_length < final_length:
+    i += 1
+    this_word = wordlist[i]
+    letters = list(this_word)
+    starting_hand = None
+    other_hand = None
+    letter_range = range(0,len(this_word))
+    odds = set()
+    evens = set()
 
-matching_words = []
-
-# {word}
-url = "https://wordsapiv1.p.rapidapi.com/words/"
-querystring = {"letterPattern":"[a-z|A-Z]{6,8}","limit":"20"}
-headers = {
-	"X-RapidAPI-Key": "ad33b108dcmsh3723a6bc9c1280fp11440ajsn440004b087de",
-	"X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
-}
-response = requests.get(url, headers=headers, params=querystring)
-print(response.json())
-print(type(response))
-print()
-
-
-
-
-# TODO: Use the API to pull in a list of dictionary words
-# TODO: Create configuration options for word selection: word length min/max, include/exclude string, ?
-# TODO: Create configuration options for list: Length, ?
-
-
-
-
-""" for letter_index in letters:
-    if letter_index %2 == 0:
-        if this_word[letter_index] not in starting hand:
-            return false
-"""
-
-
-
-
-def alternate_hands_check(word, starting_hand, other_hand):
-    this_word = word
-    letter_range = range(0,len(word))
-    odds = []
-    evens = []
+    if letters[0].lower() in lefthand:
+        starting_hand = lefthand
+        other_hand = righthand
+    elif letters[0].lower() in righthand:
+        starting_hand = righthand
+        other_hand = lefthand
+    else:
+        print("Please supply only letters.")
 
     for letter_index in letter_range:
-        if letter_index % 2 != 0:
-            odds.append(this_word[letter_index])
-
-        elif letter_index % 2 == 0:
-            evens.append(this_word[letter_index])
-
+        if letter_index %2 == 0:
+            evens.add(this_word[letter_index])
+        else:
+            odds.add(this_word[letter_index])
 
     all_evens = all(x in starting_hand for x in evens)
     all_odds = all(y in other_hand for y in odds)
@@ -60,51 +44,20 @@ def alternate_hands_check(word, starting_hand, other_hand):
     if not all_evens or not all_odds:
         # print("{} is NOT a matching word!".format(this_word))
         pass
-
     elif all_evens and all_odds:
-        matching_words.append(this_word)
-
+        sentence.append(this_word)
+        cur_sentence_length += 1
     else:
         print("What the fuck did you do??")
 
-for word in words:
-    global letters
-    letters = list(word)
-    starting_hand = None
-    other_hand = None
+print(sentence)
+# TODO: concatenate together correcthorse style
+# ''.join(listString)
 
-    if letters[0].lower() in lefthand:
-        starting_hand = lefthand
-        other_hand = righthand
+# TODO: Create configuration options for word selection: word length min/max, include/exclude string, ?
+# TODO: Create configuration options for list: Length, ?
+# TODO: Logic to concatenate strings of words with option(default?) to alternate each word's first letter (LH v RH)
+# TODO: Logic for various cases: lower, upper, random, alternating every X, pascal, ...
+# TODO: Sliding scale for semantic relation strength to previous word? (Datamuse API)
 
-    elif letters[0].lower() in righthand:
-        starting_hand = righthand
-        other_hand = lefthand
-
-    else:
-        print("Please supply only letters.")
-
-    alternate_hands_check(word, starting_hand, other_hand)
-
-print(matching_words)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" 
-
-reversedString = ''.join(listString)
-
-
- """
+# _dict_.keys()
