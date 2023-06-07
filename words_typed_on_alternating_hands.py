512999b9f8d702f541b5761d957ed9b3acb4d117
Aaron <C-Shart@users.noreply.github.com>
2023-06-06 18:01:46 -0700

    switch case cleanup

--- words_typed_on_alternating_hands.py
+++ words_typed_on_alternating_hands.py
@@ -24,16 +24,12 @@ def apply_case(word, case):
     match case:
         case "lower":
             cased_word = word.lower()
-            return cased_word
         case "UPPER":
             cased_word = word.upper()
-            return cased_word
         case "PascalCase":
             cased_word = word.capitalize()
-            return cased_word
         case "Swapped":
             cased_word = word.swapcase()
-            return cased_word
         case "sPoNgEbOb":
             # TODO: per-word or to the entire final one
             cased_word = []
@@ -41,7 +37,6 @@ def apply_case(word, case):
             for n in word:
                 spongebob_list.append(n.upper() if word.index(n) % 2 == 0 else n.lower())   # TODO: reversible
             cased_word = ''.join(spongebob_list)
-            return cased_word
         case "RanDoM":
             cased_word = []
             random_list = []
@@ -49,7 +44,7 @@ def apply_case(word, case):
                 rand = random.randrange(0, 10, 1)
                 random_list.append(n.upper() if rand > 5 else n.lower())
             cased_word = ''.join(random_list)
-            return cased_word
+    return cased_word
 
 # CONFIG: FINAL STRING
 final_length = 5                    # TODO: configurable
